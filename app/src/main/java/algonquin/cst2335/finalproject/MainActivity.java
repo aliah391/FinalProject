d979cf590c431408e28ab1b64939184ed2792f33
Loveleen <lovel0115@algonquinlive.com>
2023-07-20 22:32:46 -0400

    GUI

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -11,19 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-            // Initialize SharedPreferences
-            sharedPreferences = getSharedPreferences("QuizPreferences", MODE_PRIVATE);
-
-            // Load previous settings if available
-            int selectedTopic = sharedPreferences.getInt("selectedTopic", -1);
-            int numQuestions = sharedPreferences.getInt("numQuestions", 10);
-
-            // Add logic to handle topic selection and number of questions input
-            // Save the selected topic and number of questions in SharedPreferences
-            // Start the quiz activity with the selected topic and number of questions
         }
-
     }
