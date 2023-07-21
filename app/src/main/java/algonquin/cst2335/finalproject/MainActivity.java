b54573b61bf6c888ad0fc63ff42b8414aa9ddf1c
Irina Dontsova <dont0010@algonquinlive.com>
2023-07-20 20:12:12 -0400

    1 milestone

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,9 +1,11 @@
 package algonquin.cst2335.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import algonquin.cst2335.finalproject.databinding.ActivityMainBinding;
 
@@ -14,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+       /* new AlertDialog.Builder(this)
+                .setTitle("Conversion Result")
+                .setMessage("Converted Amount: ")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show(); */
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_from_currency);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinnerDestination = (Spinner) findViewById(R.id.spinner_to_currency);
+        ArrayAdapter<CharSequence> adapterDestination = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, android.R.layout.simple_spinner_item);
+        adapterDestination.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDestination.setAdapter(adapterDestination);
+
     }
 }
