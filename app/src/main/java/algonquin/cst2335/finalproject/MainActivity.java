0ace348e73ebe3524106b888883b5d70d48be47d
Loveleen <lovel0115@algonquinlive.com>
2023-07-20 20:33:27 -0400

    GUI

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,18 +1,28 @@
 package algonquin.cst2335.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import algonquin.cst2335.finalproject.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+
+
+
+            // Initialize SharedPreferences
+            sharedPreferences = getSharedPreferences("QuizPreferences", MODE_PRIVATE);
+
+            // Load previous settings if available
+            int selectedTopic = sharedPreferences.getInt("selectedTopic", -1);
+            int numQuestions = sharedPreferences.getInt("numQuestions", 10);
+
+            // Add logic to handle topic selection and number of questions input
+            // Save the selected topic and number of questions in SharedPreferences
+            // Start the quiz activity with the selected topic and number of questions
+        }
     }
-}
