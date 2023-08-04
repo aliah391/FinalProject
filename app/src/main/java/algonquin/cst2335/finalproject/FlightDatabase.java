ee57cfc50c14604b273ebc4049a9e41305c6ee97
Aliah Smith <aliahsmith913@gmail.com>
2023-08-04 15:27:01 -0400

    trying to fix fragment

---
+++ app/src/main/java/algonquin/cst2335/finalproject/FlightDatabase.java
@@ -0,0 +1,5 @@
+package algonquin.cst2335.finalproject;
+public class FlightDatabase {
+    public FlightDatabase() {
+    }
+}
---
+++ app/src/main/java/algonquin/cst2335/finalproject/FlightDetailsFragment.java
@@ -0,0 +1,52 @@
+package algonquin.cst2335.finalproject;
+
+import androidx.fragment.app.Fragment;
+
+
+
+
+    import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.room.Room;
+
+
+import algonquin.cst2335.finalproject.databinding.FlightdetailsfragmentBinding;
+    public class FlightDetailsFragment extends Fragment {
+        FlightDatabase fdatabase;
+        FlightDAO flightdao;
+        NameOfflight selected;
+        public FlightDetailsFragment(NameOfflight a){
+            selected= a;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            super.onCreateView(inflater, container, savedInstanceState);
+
+            FlightdetailsfragmentBinding binding = FlightdetailsfragmentBinding.inflate(inflater);
+//
+//        binding.nameOfFlightText.setText(selected.);
+//        binding.destinationText.setText(selected.);
+//        binding.gateText.setText(selected);
+//        binding.delayText.setText(selected.);
+//        binding.TerminalText.setText(selected.);
+
+            binding.saveData.setOnClickListener(clk -> {
+
+                fdatabase = Room.databaseBuilder(getContext().getApplicationContext(), FlightDatabase.class, "database-name").build();
+               // flightdao= fdatabase.flightDAO();
+            } );
+            return binding.getRoot();
+
+        }
+    }
+
+}
+
