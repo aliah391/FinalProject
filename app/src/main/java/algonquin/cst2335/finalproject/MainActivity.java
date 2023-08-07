0c248c39693b9614b79a9dce3d41be73ec59608a
IrinaDnt <133401166+IrinaDnt@users.noreply.github.com>
2023-08-06 21:06:24 -0400

    Update MainActivity.java

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,18 +1,10 @@
 package algonquin.cst2335.finalproject;
 
-import android.app.Activity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import algonquin.cst2335.finalproject.databinding.ActivityMainBinding;
-
-
 import algonquin.cst2335.finalproject.databinding.ActivityMainBinding;
 
 
@@ -41,41 +33,34 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+      super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         binding.Currency.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, CurrencyConverter.class);
             startActivity(intent);
-
+        });
 
         Button bearImageButton = findViewById(R.id.Bear_Image);
         bearImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start new NewActivity
                 Intent intent = new Intent(MainActivity.this, NewActivity.class);
                 startActivity(intent);
-
-        binding.Trivia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,QuizStartActivity.class));
-
-
-
-            ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-            setContentView(binding.getRoot());
-           // setSupportActionBar(binding.toolbar);
-        binding.Aviation.setOnClickListener(click->{
-          Intent nextPage= new Intent(MainActivity.this, Aviation.class);
-          startActivity(nextPage);
-
+            }
         });
 
+        binding.Trivia.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, QuizStartActivity.class));
+        });
 
-        }
+        binding.Aviation.setOnClickListener(click -> {
+            Intent nextPage = new Intent(MainActivity.this, Aviation.class);
+            startActivity(nextPage);
+        });
+    }
+}
 //
 //        @Override
 //        public boolean onCreateOptionsMenu(Menu menu) {
@@ -103,14 +88,3 @@ public class MainActivity extends AppCompatActivity {
 //
 //            return true;
 //        }
-
-            }
-
-        });
-    }
-
-
-}
-
-}
-
